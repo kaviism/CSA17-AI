ae24741d0d78fc389cee8c714e3d1603119e8381
kaviism <114048088+kaviism@users.noreply.github.com>
2024-01-09 14:56:21 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+    visited.add(start)
+
+    while queue:
+        node = queue.pop(0)
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+if __name__ == "__main__":
+    
+    graph = defaultdict(list)
+    graph[0] = [2, 4]
+    graph[1] = [4]
+    graph[2] = [1, 3]
+    graph[3] = [3]
+
+    print("BFS traversal:")
+    bfs(graph, 2)
